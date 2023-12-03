d5d1d6955df52a2d91346df9c314febe2f59eceb
pontus <pontus.goth@outlook.com>
2023-12-03 14:15:50 +0100

    changed project structure, updated docker-compose.yaml

--- DjangoBoard/DjangoBoard/asgi.py
+++ DjangoBoard/DjangoBoard/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangoboard project.
+ASGI config for DjangoBoard project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoboard.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoBoard.settings')
 
 application = get_asgi_application()
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Djangboard
 A django web application to monitor different devices, services, applications and more
+
+# get started 
+activate virt env using source activate
