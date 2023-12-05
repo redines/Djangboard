7d4619c111372b5d47939fadaecde7cad21839a9
pontus <pontus.goth@outlook.com>
2023-12-05 19:21:26 +0100

    added qbittorrent api, working on getting data from api

--- DjangoBoard/boardApp/views.py
+++ DjangoBoard/boardApp/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render, HttpResponse
+import qbittorrentapi
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    conn_info = dict(
+    host="http://192.168.50.43",
+    port=8080,
+    username="admin",
+    password="521986",
+    )
+
+    qbt_client = qbittorrentapi.Client(**conn_info)
+
+    try:
+        qbt_client.auth_log_in()
+    except qbittorrentapi.LoginFailed as e:
+        print(e)
+
+    listtorrent = []
+
+    with qbittorrentapi.Client(**conn_info) as qbt_client:
+        print(f"qBittorrent: {qbt_client.app.version}")
+        listtorrent.append({"ApplicationName":"Qbittorrent","Version":qbt_client.app.version})
+        for torrent in qbt_client.torrents_info():
+            new_dict = {"name":torrent.name, "state":torrent.state}
+            listtorrent.append(new_dict)
+    
+    print(listtorrent)
+    return render(request, "home.html",{'listtorrent':listtorrent})
