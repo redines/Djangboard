03703a0b4a81d9900468d689afb209230563619b
pontus <pontus.goth@outlook.com>
2023-12-11 07:31:26 +0100

    fixed html for blogg templates, added query in view to get blog posts from db

--- DjangoBoard/BlogEditor/views/views.py
+++ DjangoBoard/BlogEditor/views/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from ..models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, "BlogEditorhome.html")
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "BlogEditorhome.html",context)
